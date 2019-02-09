ef0c80745b164a432084f4038af07083031f17f0
Sharon Mertens <sharonmertens@Sharons-MacBook-Air-2.local>
2019-02-08 22:28:47 -0500

    mongoURI = process.env.MONGODB_URI

--- server.js
+++ server.js
@@ -5,29 +5,25 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const session = require('express-session');
-const port = 3000;
 const bcrypt = require('bcrypt');
+const db = mongoose.connection;
+
+// =======================================
+//             CONFIGURATION
+// =======================================
+// Allow use of Heroku's port or your own local port, depending on the environment
+const port = process.env.PORT || 3000;
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/' + 'branchforce'
 
 // middleware
 app.use(express.json());
 app.use(express.static('public'));
 
 app.use(session({
-  secret: 'branchforceforever',
+  secret: 'brandforceforever',
   resave: false,
   saveUninitialized: false
 }));
-
-app.get('/mytrips', (req, res) => {
-  if(req.session.currentUser){
-      res.json(req.session.currentUser)
-  } else {
-      res.status(401).json({
-        status: 401,
-        message: 'User not logged in!'
-      })
-  }
-})
 
 // Users controller
 const userController = require('./controllers/users.js');
@@ -43,6 +39,11 @@ app.use('/sessions', sessionsController);
 app.listen(port, () => {
   console.log(`Branch Force App listening on port: ${port}`);
 });
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', mongoURI));
+db.on('disconnected', () => console.log('mongo disconnected'));
 
 // connect to mongoose
 mongoose.connect('mongodb://localhost:27017/branchforce', { useNewUrlParser: true});
