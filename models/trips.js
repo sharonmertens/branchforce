9a768527ff15455fc19d5736135f7c985b187beb
Naftali <weiss@Naftalis-iMac.fios-router.home>
2019-02-10 14:01:54 -0500

    trips seed

---
+++ models/trips.js
@@ -0,0 +1,31 @@
+module.exports = [
+  {
+    type:'hotel',
+    price:130,
+    title:'Holiday Inn',
+    location:'London',
+    dates:[]
+  },
+  {
+    type:'hotel',
+    price:200,
+    title:'DubbleTree',
+    location:'London',
+    img:"http://ingkaoigg/png",
+    dates:[]
+  },
+  {
+    type:'flight',
+    price:300,
+    title:'Jet Blue',
+    location:'Spain',
+    dates:['November 05,2019 3:00 PM', 'November 09,2019 8:30 AM' ]
+  },
+  {
+    type:'activity',
+    price:300,
+    title:'Blue Man Group',
+    location:'Flordy',
+    dates:['November 05,2019 3:00 PM', 'November 09,2019 8:30 AM' ]
+  },
+]
