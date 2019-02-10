959adb9a7ca36e5750994770ff44dfc5f9b2ac24
Sharon Mertens <sharonmertens@Sharons-MacBook-Air-2.local>
2019-02-10 15:37:18 -0500

    london / hawaii / tokyo seed added

--- models/trips.js
+++ models/trips.js
@@ -1,31 +1,209 @@
 module.exports = [
   {
     type:'hotel',
-    price:130,
-    title:'Holiday Inn',
+    price:216,
+    title:'The Montague on The Gardens',
     location:'London',
+    img:'../public/images/hotel/montague.png',
     dates:[]
   },
   {
     type:'hotel',
-    price:200,
-    title:'DubbleTree',
+    price:264,
+    title:'The Chesterfield Mayfair',
     location:'London',
-    img:"http://ingkaoigg/png",
+    img:'../public/images/hotel/chesterfield.png',
+    dates:[]
+  },
+  {
+    type:'hotel',
+    price:85,
+    title:'Park Grand London Hyde Park',
+    location:'London',
+    img:'../public/images/hotel/parkgrand.png',
     dates:[]
   },
   {
     type:'flight',
     price:300,
     title:'Jet Blue',
-    location:'Spain',
-    dates:['November 05,2019 3:00 PM', 'November 09,2019 8:30 AM' ]
+    location:'London',
+    dates:[]
+  },
+  {
+    type:'flight',
+    price:250,
+    title:'British Airways',
+    location:'London',
+    dates:[]
+  },
+  {
+    type:'flight',
+    price:500,
+    title:'Virgin Atlantic',
+    location:'London',
+    dates:[]
   },
   {
     type:'activity',
-    price:300,
-    title:'Blue Man Group',
-    location:'Flordy',
-    dates:['November 05,2019 3:00 PM', 'November 09,2019 8:30 AM' ]
+    price:20,
+    title:'Big Ben',
+    location:'London',
+    img:'../public/images/activity/bigben.jpeg',
+    dates:[]
+  },
+  {
+    type:'activity',
+    price:20,
+    title:'Buckingham Palace',
+    location:'London',
+    img:'../public/images/activity/buckingham.jpeg',
+    dates:[]
+  },
+  {
+    type:'activity',
+    price:80,
+    title:'Cocktail Making Masterclass',
+    location:'London',
+    img:'../public/images/activity/cocktails.jpeg',
+    dates:[]
+  },
+  {
+    type:'hotel',
+    price:279,
+    title:'Prince Waikiki',
+    location:'Hawaii',
+    img:'../public/images/prince.png',
+    dates:[]
+  },
+  {
+    type:'hotel',
+    price:1700,
+    title:'Four Seasons Resort Lanai',
+    location:'Hawaii',
+    img:'../public/images/fourseasons.png',
+    dates:[]
+  },
+  {
+    type:'hotel',
+    price:483,
+    title:'Fairmont Kea Lani',
+    location:'Hawaii',
+    img:'../public/images/fairmont.png',
+    dates:[]
+  },
+  {
+    type:'flight',
+    price:200,
+    title:'United Airlines',
+    location:'Hawaii',
+    dates:[]
+  },
+  {
+    type:'flight',
+    price:250,
+    title:'Delta',
+    location:'Hawaii',
+    dates:[]
+  },
+  {
+    type:'flight',
+    price:350,
+    title:'American Airlines',
+    location:'Hawaii',
+    dates:[]
+  },
+  {
+    type:'activity',
+    price:135,
+    title:'Molokini and Turtle Arches Snorkeling Trip',
+    location:'Hawaii',
+    img:'../public/images/activity/snorkeling.png',
+    dates:[]
+  },
+  {
+    type:'activity',
+    price:200,
+    title:'Na Pali Experience',
+    location:'Hawaii',
+    img:'../public/images/activity/napali.png',
+    dates:[]
+  },
+  {
+    type:'activity',
+    price:80,
+    title:'Zipline',
+    location:'Hawaii',
+    img:'../public/images/activity/zipline.png',
+    dates:[]
+  },
+  {
+    type:'hotel',
+    price:166,
+    title:'The Royal Park Hotel Tokyo Shiodome',
+    location:'Tokyo',
+    img:'../public/images/hotel/royalpark.png',
+    dates:[]
+  },
+  {
+    type:'hotel',
+    price:74,
+    title:'Dai-Ichi Hotel Ryogoku',
+    location:'Tokyo',
+    img:'../public/images/hotel/dai.png',
+    dates:[]
+  },
+  {
+    type:'hotel',
+    price:109,
+    title:'Shinjuku Granbell Hotel',
+    location:'Tokyo',
+    img:'../public/images/hotel/shinjuku.png',
+    dates:[]
+  },
+  {
+    type:'flight',
+    price:867,
+    title:'United Airlines',
+    location:'Tokyo',
+    dates:[]
+  },
+  {
+    type:'flight',
+    price:426,
+    title:'Japan Airlines',
+    location:'Tokyo',
+    dates:[]
+  },
+  {
+    type:'flight',
+    price:500,
+    title:'American Airlines',
+    location:'Tokyo',
+    dates:[]
+  },
+  {
+    type:'activity',
+    price:30,
+    title:'Tokyo Skytree',
+    location:'Tokyo',
+    img:'../public/images/activity/skytree.jpeg',
+    dates:[]
+  },
+  {
+    type:'activity',
+    price:18,
+    title:'Meiji Jingu',
+    location:'Tokyo',
+    img:'../public/images/activity/skytree.jpeg',
+    dates:[]
+  },
+  {
+    type:'activity',
+    price:200,
+    title:'Odaiba Entertainment Island',
+    location:'Tokyo',
+    img:'../public/images/activity/odaiba.jpeg',
+    dates:[]
   },
 ]
